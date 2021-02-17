ecbdc7ce9bd0bf45aba278e8e43e6999b8dfbbcb
kucukkanat <htolgasahin@gmail.com>
2021-02-17 15:57:22 +0100

    feat: added some logic to socket server

--- packages/server/src/Server.ts
+++ packages/server/src/Server.ts
@@ -3,9 +3,8 @@ import http from "http";
 import { SERVER_PORT } from "../../config";
 import * as WebSocket from "ws";
 import Emitter from "@hackdonalds/emitter";
-import { Channel, ClientMessage, ServerMessage, User } from "@pandachat/core";
+import { Channel, User } from "@pandachat/core";
 import { SocketMessage } from "@pandachat/core/types";
-type SocketConnection = WebSocket & User
 
 export class Server {
   _server: http.Server;
@@ -13,10 +12,12 @@ export class Server {
   wss: WebSocket.Server;
   _eventbus: Emitter;
   channels: Channel[];
+  users: User[];
   constructor() {
     this.app = express();
     this._eventbus = new Emitter();
     this.channels = [];
+    this.users = []
     //initialize a simple http server
     this._server = http.createServer(this.app);
     this.wss = new WebSocket.Server({ server: this._server });
@@ -34,37 +35,61 @@ export class Server {
     this._server.close();
   }
   initSocketHandler() {
-    this.wss.on("connection", (ws: SocketConnection) => {
-
+    this.wss.on("connection", (ws) => {
+      // After a user is connected to create an actual user instance we wait for the declaration
+      let user: User | null = null
       ws.on("message", (data: string) => {
-        const parsedMessage: ClientMessage = JSON.parse(
+        const message: SocketMessage = JSON.parse(
           data
         ) as SocketMessage;
-        
-        Object.keys(parsedMessage)
-        .forEach(key => {
-          eventTriggers[key as keyof SocketMessage](ws,parsedMessage)
-        })
+
+        if (message.type === "user:declare") {
+          const foundUser = this.getUserByUsername(message.payload.name)
+          if (foundUser) {
+            ws.send(JSON.stringify({
+              type: "error:user_exists",
+              message: `A user with username ${message.payload.name} already exists`
+            }))
+          } else {
+            user = new User(message.payload.name)
+            user.attachWebSocketConnection(ws)
+            this.userOnline(user)
+            user.send({
+              type: "user:created",
+              payload: { id: user.id }
+            })
+          }
+        }
+
       });
       ws.send("pong!");
     });
   }
-  send(ws: SocketConnection, message: ServerMessage) {
-    ws.send(JSON.stringify(message))
+  getUserByUsername(username: string) {
+    return this.users.find(u => u.name === username)
+  }
+  createChannel(name: string) {
+    const newChannel: Channel = new Channel(name)
+    this.channels.push(newChannel)
+    return newChannel
+  }
+  getOrCreateChannel(channelName: string): Channel {
+    const foundChannel = this.channels.find(channel => channel.name == channelName)
+    if (foundChannel) {
+      return foundChannel
+    } else {
+      return this.createChannel(channelName)
+    }
+  }
+  userOnline(user: User) {
+    this.users.push(user)
+    return user
+  }
+  userOffline(user: User) {
+    this.users = this.users.filter(u => u.name !== user.name)
+    return this.users
   }
 }
 
 // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#example-18
 // https://golang.org/doc/effective_go#blank
-type HandlerFN = (ws:SocketConnection, message: ClientMessage)=>void
-type ET = { [k in keyof Partial<SocketMessage>]: HandlerFN }
-const eventTriggers: ET = {
-  "user:declare": (ws:SocketConnection,message:SocketMessage) => {
-    ws.username = message["user:declare"].username
-  },
-  "channel.users:list": () => { },
-  "channel:close": () => { },
-  "channel:open": () => { },
-  "user.speaking:start": () => { },
-  "user.speaking:stop": () => { },
-}
