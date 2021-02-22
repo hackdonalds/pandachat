8e0e9684110bb1d4d7151574f86d246239bfb684
kucukkanat <htolgasahin@gmail.com>
2021-02-22 02:06:42 +0100

    feat: put together server and client with types

--- packages/client/src/SocketClient.ts
+++ packages/client/src/SocketClient.ts
@@ -1,25 +1,26 @@
-import { Emitter } from "./lib/Emitter.js"
-/**
- * @typedef {Object} SocketClientOpts
- * @property {string} username
- * @property {string} channelName
- * @property {string} url
- */
+import Emitter from "@hackdonalds/emitter"
+import * as SocketMessageTypes from "../../server/src/SocketMessageTypes"
+import { User } from "../../server/src/User"
+type SocketClientOpts = {
+    username: string
+    channelName: string
+    url?: string
+}
 
 const { host, hash } = window.location
-export class SocketClient extends Emitter {
-    /**
-     * 
-     * @param {SocketClientOpts} opts 
-     */
+export class SocketClient extends Emitter<"init" | "open" | "close" | "message" | "initialized" | "error"> {
+    username: string
+    channelName: string
+    connection: WebSocket
+    channelUsers: Array<ReturnType<User["toJSON"]>>
     constructor({
         username, channelName, url
-    }) {
+    }: SocketClientOpts) {
         super()
         this.username = username
         this.channelName = channelName
         this.connection = new WebSocket(url || `ws://${host}/ws`)
-
+        this.channelUsers = []
         this.connection.onmessage = ({ data }) => this.emit("message", JSON.parse(data))
 
         this.connection.onclose = () => {
@@ -38,12 +39,12 @@ export class SocketClient extends Emitter {
             })
             this.emit("initialized")
         })
+        this.on("message", (data: SocketMessageTypes.UserJoined | SocketMessageTypes.UserLeft) => {
+            console.log("User joined:", data.payload)
+        })
     }
-    /**
-     * 
-     * @param {any} data 
-     */
-    send(data) {
+
+    send(data: SocketMessageTypes.InitEvent) {
         this.connection?.send(JSON.stringify(data))
     }
     close() {
